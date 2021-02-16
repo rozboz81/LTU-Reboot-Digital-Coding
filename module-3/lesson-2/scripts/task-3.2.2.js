0c480622c811cf4586eb687e7b499115f345ac0b
rozboz81 <roy.barrow@icloud.com>
2021-02-16 19:42:49 +0000

    update task 3.2.2

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,13 @@
+function getFullName(firstName,lastName){
+    let fullName = firstName + ' ' + lastName;
+    console.log(fullName);
+}
+
+var fName = 'Phil';
+var lName = 'Barlow';
+
+var fName2 = 'Roy';
+var lName2 = 'Barrow';
+
+getFullName(fName,lName);
+getFullName(fName2,lName2);
