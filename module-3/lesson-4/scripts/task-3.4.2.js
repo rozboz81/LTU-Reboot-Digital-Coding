529d766a4a305ad680cf842cf0165d2b755101b6
rozboz81 <roy.barrow@icloud.com>
2021-02-23 20:09:41 +0000

    update task 3.4.2

---
+++ module-3/lesson-4/scripts/task-3.4.2.js
@@ -0,0 +1,7 @@
+var arrFavouriteFoods = ['Lasagne','Fish & Chips','Bacon Butties','Pizza','Avocado']
+    
+for(var counter = 0; counter < arrFavouriteFoods.length; counter++){
+    let food = arrFavouriteFoods[counter];
+
+    console.log(counter + ': ' + food);
+}
