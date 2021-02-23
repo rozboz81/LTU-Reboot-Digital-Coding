8991e760b63fdb0563d5dfebd8e473bfbfeb0b41
rozboz81 <roy.barrow@icloud.com>
2021-02-23 20:38:00 +0000

    update task 3.4.5

---
+++ module-3/lesson-4/scripts/task-3.4.5.js
@@ -0,0 +1,33 @@
+objRecipe = {
+    recipeTitle:'Beans on Toast',
+    servings: 1,
+    ingredients: [
+        'Baked Beans',
+        'Bread',
+        'Butter',
+        'Black Pepper'
+    ],
+    directions:[
+        'Empty tin of beans into a pan',
+        'Put bread in toaster and toast until golden brown',
+        'Butter the toast',
+        'Place beans on top of buttered toast',
+        'Apply black pepper as you see fit'
+    ],
+    letsCook: function(){
+        console.log("I'm hungry, let's cook " + this.recipeTitle);
+    },
+};
+
+console.log(objRecipe.recipeTitle);
+console.log(objRecipe.servings);
+for(counter = 0; counter < objRecipe.ingredients.length; counter++){
+    let ingredient = objRecipe.ingredients[counter];
+    console.log(ingredient);
+}
+for(counter = 0; counter < objRecipe.directions.length; counter++){
+    let direction = objRecipe.directions[counter];
+    console.log(direction);
+} ;
+
+objRecipe.letsCook();
