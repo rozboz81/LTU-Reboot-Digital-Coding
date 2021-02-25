7258764aee971fb011bcc7fe21e31f3f8ece8572
rozboz81 <roy.barrow@icloud.com>
2021-02-25 21:03:27 +0000

    Update JS Shopping list with comments

--- module-3/lesson-5/scripts/shoppingCart.js
+++ module-3/lesson-5/scripts/shoppingCart.js
@@ -59,28 +59,45 @@ let arrShoppingCart = [
   quantity:3,
   price:.4
 }];
-
+/**
+ * Returns x raised to the n-th power.
+ *
+ * @param {array} arrShoppingCart The Shopping Cart array.
+ * @param {object} objCoupon A coupon object.
+ * @return {number} x the total price of the cart with discounts applied
+ */
 function getTotalPrice(arrShoppingCart=[],objCoupon={}){
+  // setting total of cart to 0 before the loop starts
   let total = 0;
+  // setting the item price to 0 so it isn't redeclared in the loop
   let itemPrice = 0;
   for(var counter=0; counter < arrShoppingCart.length; counter++){
     // console.log(counter);
+    // select the current cart item using the counter as the array key
     var objItem = arrShoppingCart[counter];
     // console.log(objItem);
+    // muliltply the cost of the item by the quanity of the item
     itemPrice = parseFloat(objItem.quantity) * parseFloat(objItem.price);
     // console.log(itemPrice);
+    // if a coupon has been passed as an argument AND the coupon type is percentage
     if(objCoupon && objCoupon.type == 'percentage'){
+      // if the coupon category = the current item type OR coupon category = all
       if(objCoupon.category == objItem.type || objCoupon.category == 'all'){
+        // itemPrice = percentage decrease for item
         itemPrice = (itemPrice/100) * (100 - objCoupon.value);
         // console.log(itemPrice);
       } 
     }
+    // total = old total + current item price
     total = total + itemPrice;
     // console.log(total);
   }
+  // if a coupon has been passed as an argument AND the coupon type is total
   if(objCoupon && objCoupon.type == 'total'){
+    // decrease the total by the value of the coupon
     total = total - objCoupon.value;
   }
+  // return the total price of the cart (with the discount applied where applciable)
   return total.toFixed(2);
 }
 let objCoupon = {
