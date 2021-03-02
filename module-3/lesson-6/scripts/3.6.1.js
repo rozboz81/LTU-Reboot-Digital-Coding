be6618344e21f782bcf316f59c79b45722de24c8
rozboz81 <roy.barrow@icloud.com>
2021-03-02 19:42:45 +0000

    task 3.6.1 update

---
+++ module-3/lesson-6/scripts/3.6.1.js
@@ -0,0 +1,5 @@
+var element = document.querySelector('.selectMe');
+console.log(element);
+element.setAttribute('alex','example');
+var h1 = document.querySelector("h1");
+h1.style.fontSize = '100px';
