a279777d523329314a550174a1c8a681d7a84570
rozboz81 <roy.barrow@icloud.com>
2021-03-04 20:58:06 +0000

    task 3.7.1 update

--- module-3/lesson-7/scripts/task-3.7.1.js
+++ module-3/lesson-7/scripts/task-3.7.1.js
@@ -1,1 +1,33 @@
-console.log("Roy is cool;")
+/*----Variables----*/
+const arrAnimalFilters = document.querySelectorAll('.animalFilter');
+const arrAnimalImages = document.querySelectorAll('.animalImage');
+//console.log(arrAnimalFilters);
+
+/*---- Event Listeners----*/
+for(counter=0; counter < arrAnimalFilters.length; counter++){
+    // console.log(counter);
+    let currentFitler = arrAnimalFilters[counter];
+    // console.log(currentFilter);
+    currentFitler.addEventListener('click',filterImages);
+}
+
+/*----Functions----*/
+function filterImages(event){
+    // console.log(event.target.innerHTML);
+    var animalName = event.target.innerHTML.toLowerCase();
+    // console.log(animalName);
+    for(counter=0; counter < arrAnimalImages.length; counter++){
+        let currentImage = arrAnimalImages[counter];
+        currentImage.style.display = 'none';
+        // console.log(currentImage);
+        if(animalName == 'all'){
+            currentImage.style.display = 'inline';
+        } else {
+            let currentImageAnimal = currentImage.getAttribute('animal');
+            // console.log(currentImageAnimal);
+            if(currentImageAnimal == animalName){
+                currentImage.style.display = 'inline';
+            }
+        }
+    }
+}
